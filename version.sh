612420a0545179f2efccc21b60d121d15ec080b0
Paweł Zaremba <pawzar@gmail.com>
2022-07-14 16:41:30 +0200

    Update versioning

---
+++ version.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+# bump RC version
+_VERSION=$(semver -i prerelease --preid rc "$(head -1 ./version | tr -d '\n')") && tee <<<"$_VERSION" ./version
+
+# Describe change in a commit
+git commit -m "Bump version to 'v$(head -1 ./version | tr -d '\n')'" ./version
+
+# Tag version
+git tag "v$(head -1 ./version | tr -d '\n')"
+
+# Push to origin
+git push --atomic origin "$(git rev-parse --abbrev-ref HEAD)" "v$(head -1 ./version | tr -d '\n')"
