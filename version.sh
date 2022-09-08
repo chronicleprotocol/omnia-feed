889e0166620721bcaa578e35239571b2d9436f78
Paweł Zaremba <6050167+teghnet@users.noreply.github.com>
2022-09-08 17:16:40 +0200

    Remove StarkWare dependencies (#22)
    * Remove StarkWare dependencies
    
    * Bump version to 'v1.12.0-dev.0'
    
    * Bump version to 'v1.12.0-dev.1'
    
    * Add postPriceHook
    
    * Bump version to 'v1.12.0-dev.2'
    
    * Update deps
    
    * Bump version to 'v1.12.0-dev.3'
    
    * Update deps
    
    * update installer
    
    * update installer
    
    * Update dependencies
    
    * Update dependencies
    
    * Update dependencies
    
    * Update dependencies
    
    * Bump version to 'v1.12.0-dev.5'
    
    * Update dependencies
    
    * Update dependencies
    
    * Bump version to 'v1.12.0-dev.6'
    
    * Update dependencies
    
    * Update dependencies
    
    * Update dependencies
    
    * Revert UNI Removal

--- version.sh
+++ version.sh
@@ -4,7 +4,7 @@ set -xeuo pipefail
 # bump to next minor
 _VERSION=$(semver -i minor "$(head -1 ./version | tr -d '\n')") && tee <<<"$_VERSION" ./version
 
-# bump to next
+# bump to next patch
 _VERSION=$(semver -i "$(head -1 ./version | tr -d '\n')") && tee <<<"$_VERSION" ./version
 
 # bump RC version
@@ -18,3 +18,35 @@ git tag "v$(head -1 ./version | tr -d '\n')"
 
 # Push to origin
 git push --atomic origin "$(git rev-parse --abbrev-ref HEAD)" "v$(head -1 ./version | tr -d '\n')"
+
+# RC version bump & tag & push
+_VERSION=$(semver -i prerelease --preid rc "$(head -1 ./version | tr -d '\n')") && tee <<<"$_VERSION" ./version \
+&& git commit -m "Bump version to 'v$(head -1 ./version | tr -d '\n')'" ./version \
+&& git tag "v$(head -1 ./version | tr -d '\n')" \
+&& git push --atomic origin "$(git rev-parse --abbrev-ref HEAD)" "v$(head -1 ./version | tr -d '\n')"
+
+# PATCH version bump & tag & push
+_VERSION=$(semver -i "$(head -1 ./version | tr -d '\n')") && tee <<<"$_VERSION" ./version \
+&& git commit -m "Bump version to 'v$(head -1 ./version | tr -d '\n')'" ./version \
+&& git tag "v$(head -1 ./version | tr -d '\n')" \
+&& git push --atomic origin "$(git rev-parse --abbrev-ref HEAD)" "v$(head -1 ./version | tr -d '\n')"
+
+# BUMP DEV
+_VERSION=$(semver -i prerelease --preid dev "$(head -1 ./version | tr -d '\n')") && tee <<<"$_VERSION" ./version \
+&& git commit -m "Bump version to 'v$(head -1 ./version | tr -d '\n')'" ./version ./nix/sources.json \
+&& git push --atomic origin "$(git rev-parse --abbrev-ref HEAD)"
+
+# NEW MINOR DEV - wen new feature
+_VERSION=$(semver -i preminor --preid dev "$(head -1 ./version | tr -d '\n')") && tee <<<"$_VERSION" ./version \
+&& git commit -m "Bump version to 'v$(head -1 ./version | tr -d '\n')'" ./version ./nix/sources.json \
+&& git push --atomic origin "$(git rev-parse --abbrev-ref HEAD)"
+
+# -i --increment [<level>]
+#        Increment a version by the specified level.  Level can
+#        be one of: major, minor, patch, premajor, preminor,
+#        prepatch, or prerelease.  Default level is 'patch'.
+#        Only one version may be specified.
+#
+# --preid <identifier>
+#        Identifier to be used to prefix premajor, preminor,
+#        prepatch or prerelease version increments.
