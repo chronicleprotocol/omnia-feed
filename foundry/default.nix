4a3210fac8d439aea73df9dae40696adecb4d122
Paweł Zaremba <pawzar@gmail.com>
2022-11-17 22:28:38 +0100

    Update version

--- foundry/default.nix
+++ foundry/default.nix
@@ -1,35 +1,31 @@
-#{ pkgs }: with pkgs; rustPlatform.buildRustPackage {
-#  pname = "foundry";
-#  version = "0.2.0-f523139";
-#  src = fetchFromGitHub {
-#    owner = "foundry-rs";
-#    repo = "foundry";
-#    rev = "f523139a01458a7c0a02b5e8b2f37bc856cf9055";
-#    sha256 = "sha256-64lTZlzufjWE3uGGeKF4UXPz7t4xuVWyR6RiMcM+ycs=";
-#  };
-#  cargoSha256 = "sha256-9GnsVhLhMEtJJbfE9gHjdSwdbOaZuJkjyMvjL3Xarnk=";
-#  preInstall = ''
-#    pwd
-#    ls -la
-#    set -ex
-#  '';
-#}
-
-{ pkgs }: with pkgs; stdenv.mkDerivation {
-  name = "foundry-0.2.0-f523139";
+{ pkgs }: let
+  rev = "b65d58d8f634137c61d8334ecfa81711e77496c6";
+in with pkgs; stdenv.mkDerivation rec {
+  pname = "foundry";
+  version = "0.0.0-${lib.substring 0 7 rev}-0";
   src = fetchzip {
-    url = "https://github.com/foundry-rs/foundry/releases/download/nightly-f523139a01458a7c0a02b5e8b2f37bc856cf9055/foundry_nightly_linux_amd64.tar.gz";
-    sha256 = "sha256-/WudY6Zi6LEAfEet3E9f9zGLI5nSCpS/lvUl8u54qak=";
+    url = "https://github.com/foundry-rs/foundry/releases/download/nightly-${rev}/foundry_nightly_linux_amd64.tar.gz";
+    sha256 = "sha256-2isjF7yINVrhD+aW80c37ldD0q/Obf6YhqS/JB/kRJk=";
     stripRoot = false;
   };
 
-  installPhase = ''
+  nativeBuildInputs = with pkgs; [
+    makeWrapper
+    autoPatchelfHook
+  ];
+  installPhase = let
+    path = lib.makeBinPath [ git ];
+  in ''
     mkdir -p $out/bin
-    cp $src/cast $out/bin/
+    mv cast $out/bin/
+    chmod +x $out/bin/*
+    find $out/bin -type f | while read -r x; do
+      wrapProgram "$x" --prefix PATH : "${path}"
+    done
   '';
 
-#  installCheckPhase = ''
-#    $out/bin/cast --version
-#  '';
-#  doInstallCheck = true;
+  doInstallCheck = true;
+  installCheckPhase = ''
+    $out/bin/cast --version
+  '';
 }
