f67144f00df1891e5661630c8f8627cc120e192e
Paweł Zaremba <pawzar@gmail.com>
2022-11-18 02:57:33 +0100

    Remove starkware

--- starkware/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-setup(name='stark-cli',
-      version='0.0',
-      packages=['signature'],
-      scripts=['stark_cli.py'],
-      package_data={'': ['*.json']}
-      )
