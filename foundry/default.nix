dffac73b10a0abc8a925e2b3f2d7903098f9e0e3
Paweł Zaremba <pawzar@gmail.com>
2022-11-21 19:43:27 +0100

    Commit cast binary to prevent failures when nightly build is no longer available.

--- foundry/default.nix
+++ foundry/default.nix
@@ -2,12 +2,13 @@
   rev = "cb925b100b400b27875ad0667c2bec7e8d55d89c";
 in with pkgs; stdenv.mkDerivation rec {
   pname = "foundry";
-  version = "0.0.0-${lib.substring 0 5 rev}";
-  src = fetchzip {
-    url = "https://github.com/foundry-rs/foundry/releases/download/nightly-${rev}/foundry_nightly_linux_amd64.tar.gz";
-    sha256 = "sha256-TnlcU2wt1ml5lH25jYg1ZI0BXgO9FB08Lzf5i32609c=";
-    stripRoot = false;
-  };
+  version = "0.0.0-${lib.substring 0 6 rev}";
+#  src = fetchzip {
+#    url = "https://github.com/foundry-rs/foundry/releases/download/nightly-${rev}/foundry_nightly_linux_amd64.tar.gz";
+#    sha256 = "sha256-TnlcU2wt1ml5lH25jYg1ZI0BXgO9FB08Lzf5i32609c=";
+#    stripRoot = false;
+#  };
+  src = ./.;
 
   nativeBuildInputs = with pkgs; [
     makeWrapper
@@ -16,8 +17,9 @@ in with pkgs; stdenv.mkDerivation rec {
   installPhase = let
     path = lib.makeBinPath [ git ];
   in ''
+    set -e
     mkdir -p $out/bin
-    mv cast $out/bin/
+    cp $src/cast $out/bin/
     chmod +x $out/bin/*
     find $out/bin -type f | while read -r x; do
       wrapProgram "$x" --prefix PATH : "${path}"
